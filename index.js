8a964281b66a6289d24fd58e4736f07236a79cb7
dialaay5 <diala.ay5@gmail.com>
2023-02-03 00:37:46 +0200

    upload all

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const data = [{
     City: 'Kiryat Shmona',
     Price: 3200,
     Link_of_the_store: new URL('https://big-electric.coi.co.il/search?q=tv'),
-    Rating_of_the_plac: 3.5
+    Rating_of_the_place: 3.5
 },
 {
     Id: 2,
@@ -14,8 +14,8 @@ const data = [{
     Address: 'Ashkelon',
     City: 'Ashkelon',
     Price: 3000,
-    Link_of_the_store:  new URL('https://www.superelectric.co.il/product/samsung-75qn90b'),
-    Rating_of_the_plac: 4
+    Link_of_the_store: new URL('https://www.superelectric.co.il/product/samsung-75qn90b'),
+    Rating_of_the_place: 4
 },
 {
     Id: 3,
@@ -24,7 +24,7 @@ const data = [{
     City: 'Haifa',
     Price: 3340,
     Link_of_the_store: new URL('https://www.samgalelite.co.il'),
-    Rating_of_the_plac: 1.5
+    Rating_of_the_place: 1.5
 },
 {
     Id: 4,
@@ -32,20 +32,23 @@ const data = [{
     Address: 'HaHistadrut Ave 248, Haifa',
     City: 'Haifa',
     Price: 3100,
-    Link_of_the_store:  new URL('https://www.shekem-electric.co.il/catalog/product/view/id/101004/s/271445/category/219/'),
-    Rating_of_the_plac: 5.5
+    Link_of_the_store: new URL('https://www.shekem-electric.co.il/catalog/product/view/id/101004/s/271445/category/219/'),
+    Rating_of_the_place: 5.5
 }]
+
+const low_rating_threshold = 2
+const high_rating_threshold = 4
 
 function tabelsRow() {
     for (let i = 0; i < data.length; i++) {
         $('#table').append(`<tr id="tr${i}"><td>${data[i].Id}</td>
-                            <td><img src="${i}.jpg" alt="tv1" width="80"></td>
+                                 <td><a target="_blank" href="${data[i].Link_of_the_store}"><img src="images/${i}.jpg" alt="tv" width="80"></a></td>
                                  <td>${data[i].Name_of_the_store}</td>
                                  <td>${data[i].Address}</td>
                                  <td>${data[i].City}</td>
                                  <td id="price${i}">${data[i].Price}</td>
-                                 <td><a href="${data[i].Link_of_the_store}">Visit The Store Link!</a></td>
-                                 <td id="rating${i}">${data[i].Rating_of_the_plac}</td>
+                                 <td><a target="_blank" href="${data[i].Link_of_the_store}">Visit The Store Link!</a></td>
+                                 <td id="rating${i}">${data[i].Rating_of_the_place}</td>
                                  <td><button type="button" class="btn btn-outline-danger" id="${i}" onclick="deleteClick(event)">delete</button>
                                  <td> <button class="btn btn-outline-success" type="button" data-toggle="tooltip"
                                  data-placement="top" title="Edit" data-bs-toggle="modal" data-bs-target="#myModal${i}">buy</button>
@@ -102,12 +105,12 @@ function tabelsRow() {
         )
 
 
-        let rating = $(`#rating${i}`).text()
-        console.log(rating);
-        if (rating >= 4) {
+        let rating_of_tv = $(`#rating${i}`).text()
+        console.log(rating_of_tv);
+        if (rating_of_tv >= high_rating_threshold) {
             $(`#tr${i}`).toggleClass("trgreen")
         }
-        else if (rating < 2) {
+        else if (rating_of_tv < low_rating_threshold) {
             $(`#tr${i}`).toggleClass("trred")
         }
 
@@ -121,40 +124,38 @@ function average_price() {
         sum += parseFloat(price)
     }
     avg = sum / data.length
-    console.log(avg);
+    Swal.fire('The average price of all the TVs is :' + avg)
     return (avg);
 }
 
 function best_option() {
-    const arr1 = []
-    const arr2 = []
+    const array_of_best_option = []
+    const array_of_lowest_price = []
     for (let i = 0; i < data.length; i++) {
         let rating = $(`#rating${i}`).text()
         let price = $(`#price${i}`).text()
-        arr1.push(rating)
-        arr2.push(price)
+        array_of_best_option.push(rating)
+        array_of_lowest_price.push(price)
     }
-    arr1.sort();
-    if(arr1[arr1.length - 1] != arr1[arr1.length - 2]){
-    let best_rating = arr1[arr1.length - 1]
-    let result = data.find(item => item.Rating_of_the_plac == best_rating);
-    console.log(result);
-    return result;
+    array_of_best_option.sort();
+    if (array_of_best_option[array_of_best_option.length - 1] != array_of_best_option[array_of_best_option.length - 2]) {
+        let best_rating = array_of_best_option[array_of_best_option.length - 1]
+        let result = data.find(item => item.Rating_of_the_place == best_rating);
+        Swal.fire(`<form>
+        <h2>The best TV is :</h2>
+        <label for="typeText">Name Of The Store : ${result.Name_of_the_store}</label></br>
+        <label for="typeText">City : ${result.City} </label></br>
+        <label for="typeText">Address : ${result.Address} </label></br>
+        <label for="typeText">Price : ${result.Price} </label></br>
+        <label for="typeText">Rating of the place : ${result.Rating_of_the_place} </label>`)
+        return result;
     }
 }
 
-function deleteClick(event){
+function deleteClick(event) {
     i = event.target.id
     $(`#tr${i}`).remove()
 }
 
 
 tabelsRow()
-average_price()
-best_option()
-
-
-
-
-
-
